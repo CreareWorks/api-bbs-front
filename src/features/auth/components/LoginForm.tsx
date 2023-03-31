fae53e7803fedb98dd10a8a0b956b755d66befe6
Ss1460 <99880448+Ss1460@users.noreply.github.com>
2023-04-01 03:12:36 +0900

    コンポーネント反映忘れ

--- src/features/auth/components/LoginForm.tsx
+++ src/features/auth/components/LoginForm.tsx
@@ -1,24 +1,55 @@
 import React from "react";
 import styled from "styled-components";
+import {
+    FormControl,
+    FormControlLabel,
+    FormLabel,
+    Button,
+    Typography
+} from '@mui/material'
+import Input from "../../../components/Input";
 
 import useLoginForm from "../hooks/useLoginForm";
 
+
 const LoginForm = () => {
-    
+
     // 分割代入
     const {
-            token,
-            register,
-            handleSubmit,
-            errors,
-            clickHandler
-        } = useLoginForm();
+        token,
+        register,
+        handleSubmit,
+        errors,
+        clickHandler
+    } = useLoginForm();
 
     return (
         <>
-        <h3>selector確認:{ token.token }</h3>
-        
-            <form>
+            <h3>selector確認:{token.token}</h3>
+            <FormControl >
+
+                <Input register={register} type={'email'} />
+                <Typography component={'p'}
+                    variant={'inherit'}
+                    sx={{ color: 'red' }}>
+                    {errors.email?.message}
+                </Typography>
+
+                <Input register={register} type={'password'} />
+                <Typography component={'p'}
+                    variant={'inherit'}
+                    sx={{ color: 'red' }}>
+                    {errors.password?.message}
+                </Typography>
+
+                <Button variant={'contained'} sx={{marginTop: '3%'}}
+                    onClick={handleSubmit(clickHandler)}>
+                    ログイン
+                </Button>
+
+            </FormControl>
+            {/* <form>
+                
                 <label htmlFor="mail">
                     email:　
                     <input 
@@ -42,7 +73,7 @@ const LoginForm = () => {
 
                 <button type="button" onClick={handleSubmit(clickHandler)}>ログイン</button>
             
-            </form>
+            </form> */}
         </>
     )
 }
